5d57b802ae3e93070ea9c352bc253f694dab05cb
vlashchanka <vlashchanka@evolutiongaming.com>
2020-10-12 13:19:17 +0300

    Add array methods solution, remove import

---
+++ src/_solutions/w-03/07-arrays/methods/src/arrayMethods.test.ts
@@ -0,0 +1,53 @@
+import {
+  adminLastNames, books, findFirstUserByRole, getFreshBook,
+  getLastNames, getUsersByRole,
+  sayHelloToZUsers,
+  users, usersWithRoles
+} from './arrayMethods';
+
+describe('arrayMethods', () => {
+  test('should log to console all people with names starting from Z letter', () => {
+    const mock = jest.fn();
+    const logSpy = jest.spyOn(global.console, 'log').mockImplementation(mock);
+    sayHelloToZUsers(users);
+    expect(logSpy).toBeCalledTimes(1);
+    expect(logSpy).toBeCalledWith("Hello, Zoe!");
+  });
+
+  test('should return all last names of users', () => {
+    expect(getLastNames(usersWithRoles)).toEqual([
+      "Bouley",
+      "Alnaji",
+      "Baughn",
+      "Herman",
+      "Hajek",
+      "Reid",
+      "Klabunde",
+    ]);
+  });
+
+  test('should return all last names of admins', () => {
+    expect(adminLastNames).toEqual([
+      "Bouley",
+      "Hajek",
+    ]);
+  });
+
+  test('should return first editor', () => {
+    const editor = findFirstUserByRole(usersWithRoles, "Editor");
+    expect(editor).toEqual({
+      firstName: "Michael",
+      lastName: "Herman",
+      role: "Editor",
+    });
+  });
+
+  test('should return newest book', () => {
+    const newBook = getFreshBook(books);
+    expect(newBook).toEqual({
+      name: "Magic book 4th Edition",
+      year: 2020,
+    });
+  });
+});
+

e57d24feb058035d959b4280ecabdc10f1197a9f
vlashchanka <vlashchanka@evolutiongaming.com>
2020-10-20 19:21:23 +0300

    Prepared promises task

--- src/w-03/09-async/assets/basics/src/promises.ts
+++ src/w-03/09-async/assets/basics/src/promises.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import fetch from 'node-fetch';
 import { DirectorsResponse, MoviesResponse } from './types';
 
@@ -7,41 +9,26 @@ const baseUrl = 'http://localhost:3000';
  * Write a method to get directors from the API
  */
 export function getDirectors(): Promise<DirectorsResponse[]> {
-  return fetch(`${baseUrl}/api/directors`)
-    .then((r) => r.json())
-    .catch((err) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.log(err);
-      }
-      return [];
-    });
+  return Promise.resolve([]);
 }
 
 /**
  * Write a method to transform directors response to the list of links
  */
 export function getMovieLinks(directors: DirectorsResponse[]): string[] {
-  return directors.map((d) => d.movies).flat();
+  return [];
 }
 
 /**
  * Write a utility method to fetch and parse to JSON and refactor the method getDirectors()
  */
 export function getJson<T>(url: string): Promise<T> {
-  return fetch(url).then((r) => r.json());
+  return Promise.resolve([] as unknown as T);
 }
 
 /**
  * Write a method to get director movies from the API
  */
 export function getMovies(directors: DirectorsResponse[]): Promise<MoviesResponse[]> {
-  const promises = getMovieLinks(directors).map((movieLink) => getJson<MoviesResponse>(movieLink));
-  return Promise.all(promises).then((movies) => movies).catch((err) => {
-    if (err) {
-      // eslint-disable-next-line no-console
-      console.log(err);
-    }
-    return [];
-  });
+  return Promise.resolve([]);
 }

2c03f90d0aa3fd2b4788444257773233d732f013
vlashchanka <vlashchanka@evolutiongaming.com>
2020-10-21 14:23:56 +0300

    Fix linting error

--- src/w-03/09-async/assets/basics/src/promises.test.ts
+++ src/w-03/09-async/assets/basics/src/promises.test.ts
@@ -1,4 +1,4 @@
-import {getDirectors, getDirectorsRecommendations, getMovies} from './promises';
+import { getDirectors, getDirectorsRecommendations, getMovies } from './promises';
 
 describe('promises', () => {
   test('should return list of directors', async () => {
@@ -12,24 +12,25 @@ describe('promises', () => {
   });
   test('should return fastest recommendation', async () => {
     // eslint-disable-next-line no-console
-    console.time("Getting fastest recommendation")
+    console.time('Getting fastest recommendation');
     const director = await getDirectorsRecommendations([
-        "http://localhost:3000/api/directors/recommendations/1000",
-        "http://localhost:3000/api/directors/recommendations/2000",
-        "http://localhost:3000/api/directors/recommendations/3000",
-        "http://localhost:3000/api/directors/recommendations/4000",
-        "http://localhost:3000/api/directors/recommendations/5000",
-        "http://localhost:3000/api/directors/recommendations/6000",
-        "http://localhost:3000/api/directors/recommendations/7000",
-        "http://localhost:3000/api/directors/recommendations/8000",
-        "http://localhost:3000/api/directors/recommendations/9000",
-        "http://localhost:3000/api/directors/recommendations/10000",
+      'http://localhost:3000/api/directors/recommendations/1000',
+      'http://localhost:3000/api/directors/recommendations/2000',
+      'http://localhost:3000/api/directors/recommendations/3000',
+      'http://localhost:3000/api/directors/recommendations/4000',
+      'http://localhost:3000/api/directors/recommendations/5000',
+      'http://localhost:3000/api/directors/recommendations/6000',
+      'http://localhost:3000/api/directors/recommendations/7000',
+      'http://localhost:3000/api/directors/recommendations/8000',
+      'http://localhost:3000/api/directors/recommendations/9000',
+      'http://localhost:3000/api/directors/recommendations/10000',
     ]);
-    console.timeEnd("Getting fastest recommendation")
+    // eslint-disable-next-line no-console
+    console.timeEnd('Getting fastest recommendation');
     expect(director).toEqual({
       id: 2,
-      name: "Quentin Tarantino",
-      movies: "http://localhost:3000/api/directors/2/movies"
+      name: 'Quentin Tarantino',
+      movies: 'http://localhost:3000/api/directors/2/movies',
     });
   });
 });

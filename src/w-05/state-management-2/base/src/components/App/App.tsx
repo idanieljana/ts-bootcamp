2c7d84f9618dc591aee5481d579e9cf2f22caad2
vlashchanka <vlashchanka@evolutiongaming.com>
2020-11-23 02:07:40 +0200

    Finished with redux store exercises, remove store parts

--- src/w-05/state-management-2/base/src/components/App/App.tsx
+++ src/w-05/state-management-2/base/src/components/App/App.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import styles from './App.pcss';
 import './assets/css/reset.min.css';
 import { GameOfferView } from '../GameOffer/GameOffer';
 import { PlayingCardsView } from '../PlayingCards/PlayingCards';
-import { store } from '../../stores/redux/store';
 
 export const App: React.FC = () => (
-  <Provider store={store}>
-    <div className={styles.appContainer}>
-      <div className={styles.hr} />
-      <div className={styles.app}>
-        <GameOfferView />
-        <PlayingCardsView />
-      </div>
-      <div className={styles.hr} />
+  <div className={styles.appContainer}>
+    <div className={styles.hr} />
+    <div className={styles.app}>
+      <GameOfferView />
+      <PlayingCardsView />
     </div>
-  </Provider>
+    <div className={styles.hr} />
+  </div>
 );
 export default App;
